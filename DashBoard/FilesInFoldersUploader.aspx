6ec9df65e246ebadd2198038d1933424120b34d3
Akshay Khanna <akshay9911102163@gmail.com>
2017-02-19 08:31:04 +0530

    Adding upload select move rename display code

---
+++ DashBoard/FilesInFoldersUploader.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FilesInFoldersUploader.aspx.cs" Inherits="DashBoard.FilesInFoldersUploader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 26px;
+            text-align: center;
+        }
+        .auto-style3 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="4" style="text-align: center">
+                    <asp:Label ID="lbHeading" runat="server" CssClass="auto-style1" Font-Bold="True" Font-Size="Larger" Text="Uplod file"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2"></td>
+                <td class="auto-style2">
+                    <asp:ListBox ID="ListBox1" runat="server" style="text-align: center" ></asp:ListBox>
+                </td>
+                <td class="auto-style2" colspan="2">
+                    <asp:Button ID="btMoveToRigt" runat="server" OnClick="btMoveToRigt_Click" Text="&gt;" />
+                </td>
+                <td class="auto-style2">
+                    <asp:ListBox ID="lbFinalItems" runat="server"></asp:ListBox>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3" class="auto-style3">
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+                <td style="text-align: center">
+                    <asp:Button ID="btGenerateDashboard" runat="server" Height="26px" Text="Dashboard&gt;" style="text-align: center" />
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3" class="auto-style3">
+                    <asp:Button ID="btAddFileToFolder" runat="server" OnClick="btAddFileToFolder_Click" Text="Add file" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="3" class="auto-style3">
+                    <asp:Label ID="lbMsg" runat="server" Text="Msg"></asp:Label>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="2" class="auto-style3">
+                    <asp:Button ID="btPrev" runat="server" OnClick="btPrev_Click" Text="&lt;Prev" />
+                </td>
+                <td colspan="2" class="auto-style3">
+                    <asp:Button ID="btnNextFile" runat="server" OnClick="btnNextFile_Click" Text=" Next&gt;" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ DashBoard/FilesInFoldersUploader.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DashBoard
+{
+    public partial class FilesInFoldersUploader : System.Web.UI.Page
+    {
+        String pathSource = @"F:\PrevData\";
+        String pathDestination = @"F:\CurrData\";
+        string[] fileNamesList= new string[] { "checkstyle_report", "findbugs", "interpret", "JPetStore_CD_changelog", "PMD_Report" };
+        static string[] filesAdded = new string[5];
+        static int count = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    GetFilesFromFolderInListBox();
+                    //addNamesToFiles();
+                    lbHeading.Text = "Upload or choose " + fileNamesList[count] + " File";
+                }
+            }
+            catch (Exception er)
+            {
+               lbMsg.Text = er.Message;
+            }
+
+        }
+
+        //private void addNamesToFiles()
+        //{
+        //    fileNamesList= 
+
+        //}
+
+        void GetFilesFromFolderInListBox()
+        {
+            try
+            {
+                string[] fileEntries = Directory.GetFiles(pathSource+ fileNamesList[count]+@"\");
+                ListBox1.Items.Clear();
+                foreach (string fileName in fileEntries)
+                {
+                    ListBox1.Items.Add(fileName.ToString());
+                     
+                }
+                //to display files in destination folder
+                //fileEntries = Directory.GetFiles(pathDestination);
+                //lbFinalItems.Items.Clear();
+                //foreach (string fileName in fileEntries)
+                //{
+                //    lbFinalItems.Items.Add(fileName.ToString());
+                //}
+
+                lbFinalItems.Items.Clear();
+                foreach (string fileName in filesAdded)
+                {
+                    if(fileName!=null && fileName.Length>0)
+                    lbFinalItems.Items.Add(fileName.ToString());
+                }
+            }
+            catch (Exception er)
+            {
+
+                lbMsg.Text = er.Message;
+            }
+        }
+
+        protected void btAddFileToFolder_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                // Call a helper method routine to save the file.
+                SaveFile(FileUpload1.FileName);
+                Response.Redirect(Request.RawUrl);
+            }
+            else
+                // Notify the user that a file was not uploaded.
+                lbMsg.Text = "You did not browse a file to upload.";
+
+            
+        }
+
+        private void SaveFile(String postedFile)
+        {
+            try
+            {
+                String savePath = pathSource + postedFile.ToString();
+                FileUpload1.SaveAs(savePath);
+            }
+            catch (Exception er)
+            {
+                lbMsg.Text = er.Message;
+            }
+        }
+
+        protected void btMoveToRigt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //var temp = ListBox1.SelectedItem;
+                //lbMsg.Text = "Data "+ListBox1.se
+                //lbMsg.Text = "Data " + ListBox1.Items[ListBox1.SelectedIndex];
+                String fileToMoveWithpath="";
+                for (int i = 0; i < ListBox1.Items.Count; i++)
+                {
+                    if (ListBox1.Items[i].Selected)
+                    {
+                        lbMsg.Text = "Copy & renaming file " + ListBox1.Items[i].Text.ToString();
+                        fileToMoveWithpath = ListBox1.Items[i].Text.ToString();
+                    }
+                }
+                
+                // To copy a file to another location and 
+                // overwrite the destination file if it already exists.
+                System.IO.File.Copy(fileToMoveWithpath, pathDestination+ fileNamesList[count]+".xml", true);
+                filesAdded[count] = fileToMoveWithpath.ToString();
+            }
+            catch (Exception er)
+            {
+                lbMsg.Text = er.Message;
+            }
+        }
+
+        protected void btnNextFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count++;
+                if(count>=fileNamesList.Length)
+                {
+                    count = 0;
+                }
+                lbHeading.Text = "Upload or choose " + fileNamesList[count] + " File";
+                GetFilesFromFolderInListBox();
+            }
+            catch (Exception er)
+            {
+                lbMsg.Text = er.Message;
+            }
+        }
+
+        protected void btPrev_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count--;
+                if (count <0)
+                {
+                    count = fileNamesList.Length-1;
+                }
+                lbHeading.Text = "Upload or choose " + fileNamesList[count] + " File";
+                GetFilesFromFolderInListBox();
+            }
+            catch (Exception er)
+            {
+                lbMsg.Text = er.Message;
+            }
+        }
+
+
+        //protected void ListBox1_SelectedIndexChanged1(object sender, EventArgs e)
+        //{
+        //    // Get the currently selected item in the ListBox.
+        //    curItem = ListBox1.SelectedItem.ToString();
+        //    lbMsg.Text = "papu " + curItem;
+        //    // Find the string in ListBox2.
+        //}
+    }
+}
